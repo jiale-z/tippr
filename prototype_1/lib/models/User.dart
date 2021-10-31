eb49573862bf21d37052a96a086e8826da43ca0d
jiale-z <jialez.zheng@gmail.com>
2021-10-31 13:15:00 -0400

    amplify cli install

--- prototype_1/lib/models/User.dart
+++ prototype_1/lib/models/User.dart
@@ -13,7 +13,11 @@
 * permissions and limitations under the License.
 */
 
-// ignore_for_file: public_member_api_docs
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
 
 import 'ModelProvider.dart';
 import 'package:amplify_datastore_plugin_interface/amplify_datastore_plugin_interface.dart';
@@ -128,7 +132,7 @@ class User extends Model {
         : null;
   
   Map<String, dynamic> toJson() => {
-    'id': id, 'serverCode': _serverCode, 'name': _name, 'DiningPreferences': _DiningPreferences?.map((e) => e?.toJson())?.toList(), 'CustomerLinks': _CustomerLinks?.map((e) => e?.toJson())?.toList()
+    'id': id, 'serverCode': _serverCode, 'name': _name, 'DiningPreferences': _DiningPreferences?.map((DiningPreference? e) => e?.toJson()).toList(), 'CustomerLinks': _CustomerLinks?.map((Link? e) => e?.toJson()).toList()
   };
 
   static final QueryField ID = QueryField(fieldName: "user.id");
