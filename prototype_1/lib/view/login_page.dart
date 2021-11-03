c2539772ed7c2e587aa3fc7ac02d5f2f30382e91
radheshamin <radheshamin1999@gmail.com>
2021-11-03 00:17:06 -0400

    login and registration

--- prototype_1/lib/view/login_page.dart
+++ prototype_1/lib/view/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './home_page.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -19,21 +21,104 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginDemo(),
+    );
+  }
+}
+
+class LoginDemo extends StatefulWidget {
+  @override
+  _LoginDemoState createState() => _LoginDemoState();
+}
+
+class _LoginDemoState extends State<LoginDemo> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Login Page!'),
+        title: Text("Login Page"),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'p4 login page',
-              style: TextStyle(fontSize: 50),
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.height * 0.70,
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  child: Text(
+                    'Login to TIPPR',
+                    style: TextStyle(fontSize: 50),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
             ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.10,
+              width: MediaQuery.of(context).size.height * .85,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  login();
+                  // Navigator.push(
+                  //     context, MaterialPageRoute(builder: (_) => HomePage()));
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.20,
+            ),
+            Text('New User? Create Account')
           ],
         ),
       ),
     );
+  }
+
+  login() {
+    //call viewmodel here
+
+    Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
   }
 }
--- prototype_1/lib/view/registration_page.dart
+++ prototype_1/lib/view/registration_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import './home_page.dart';
+import 'package:flutter/widgets.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class RegistrationPage extends StatefulWidget {
   @override
@@ -19,21 +22,122 @@ class _RegistrationPageState extends State<RegistrationPage> {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RegistrationDemo(),
+    );
+  }
+}
+
+class RegistrationDemo extends StatefulWidget {
+  @override
+  _RegistrationDemoState createState() => _RegistrationDemoState();
+}
+
+class _RegistrationDemoState extends State<RegistrationDemo> {
+  @override
+  Widget build(BuildContext context) {
+    List<ListItem> _userroles = [
+      ListItem(id: 1, name: 'Customer'),
+      ListItem(id: 2, name: 'Server'),
+      ListItem(id: 3, name: 'Restaurant Representative')
+    ];
+    List<Object?> _selecteduserroles;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Registration Page!'),
+        title: Text("Registration Page"),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'p4 registration page',
-              style: TextStyle(fontSize: 50),
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.height * 0.70,
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  child: Text(
+                    'Register for TIPPR',
+                    style: TextStyle(fontSize: 50),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.10,
+              width: MediaQuery.of(context).size.height * .85,
+              child: MultiSelectDialogField(
+                title: Text("Select User Roles"),
+                items: _userroles
+                    .map((e) => MultiSelectItem<ListItem>(e, e.name))
+                    .toList(),
+                listType: MultiSelectListType.CHIP,
+                onConfirm: (values) {
+                  _selecteduserroles = values;
+                },
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.10,
+              width: MediaQuery.of(context).size.height * .85,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  registration();
+                  // Navigator.push(
+                  //     context, MaterialPageRoute(builder: (_) => HomePage()));
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
+  registration() {
+    //call viewmodel here _selecteduserroles has data
+
+    //need to change paths later
+    Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
+  }
+}
+
+class ListItem {
+  final int id;
+  final String name;
+
+  ListItem({
+    required this.id,
+    required this.name,
+  });
 }
