7f9522974a3b59cbd8c7fbe8a1e22d1e90b71cf1
radheshamin <radheshamin1999@gmail.com>
2021-11-04 18:12:22 -0400

    added slider to customer page

--- prototype_1/lib/view/customer_page.dart
+++ prototype_1/lib/view/customer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './home_page.dart';
 
 class CustomerPage extends StatefulWidget {
   @override
@@ -11,6 +12,9 @@ class CustomerPage extends StatefulWidget {
 }
 
 class _CustomerPageState extends State<CustomerPage> {
+  double level = 0;
+  double newLevel = 0;
+
   @override
   void initState() {
     super.initState();
@@ -21,19 +25,87 @@ class _CustomerPageState extends State<CustomerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Customer Page!'),
+        title: Text('Customer Registration'),
+        automaticallyImplyLeading: false,
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'p4 customer page',
-              style: TextStyle(fontSize: 50),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  child: Text(
+                    'Enter Customer Information',
+                    style: TextStyle(fontSize: 50),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Center(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.025,
+                ),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.10,
+              width: MediaQuery.of(context).size.width * 1,
+              child: Slider(
+                value: level,
+                min: 0,
+                max: 5,
+                divisions: 5,
+                label: "$level",
+                onChanged: (newLevel) {
+                  setState(() => level = newLevel);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Center(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.025,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.125,
+                width: MediaQuery.of(context).size.width * 1,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () {
+                    login();
+                    // Navigator.push(
+                    //     context, MaterialPageRoute(builder: (_) => HomePage()));
+                  },
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  login() {
+    //call viewmodel here
+
+    Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
   }
 }
